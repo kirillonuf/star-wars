dc2824733413126a0587ef18361217058a05702f
Kirill Onufriychuk <onufk1989@gmail.com>
2023-10-17 20:21:07 +0300

    deleted wigth

--- src/UI/Video/Video.jsx
+++ src/UI/Video/Video.jsx
@@ -12,7 +12,7 @@ const Video = ({ src, classes, playbackRate = 1.0 }) => {
   return (
     <iframe className={styles.shell}>
       <video
-      width="240" height="240"
+      
         controls={false}
         ref={videoRef}
         loop
