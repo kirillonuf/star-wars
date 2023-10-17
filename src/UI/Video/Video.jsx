45c88a5319949b234e9e5706143886daeb6c3d3c
Kirill Onufriychuk <onufk1989@gmail.com>
2023-10-17 20:37:24 +0300

    replace video with a gif

--- src/UI/Video/Video.jsx
+++ src/UI/Video/Video.jsx
@@ -1,29 +1,32 @@
 import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
+import black_hole from './video/black-hole-animation.gif'
+
 
 import styles from "./Video.module.css";
 
 const Video = ({ src, classes, playbackRate = 1.0 }) => {
-  const videoRef = useRef(null);
-  useEffect(() => {
-    videoRef.current.playbackRate = playbackRate;
-  }, [playbackRate]);
+  // const videoRef = useRef(null);
+  // useEffect(() => {
+  //   videoRef.current.playbackRate = playbackRate;
+  // }, [playbackRate]);
   return (
-    <iframe className={styles.shell}>
-      <video
-      
+    <div className={styles.shell}>
+
+      <img className={styles.video} src={black_hole} alt="black-hole"  />
+      {/* <video
         controls={false}
         ref={videoRef}
         loop
         autoPlay
-        playsinline
         muted
         className={cn(styles.video, classes)}
       >
         <source src={src} type="video/mp4" />
-      </video>
-    </iframe>
+      </video> */}
+
+    </div>
   );
 };
 
