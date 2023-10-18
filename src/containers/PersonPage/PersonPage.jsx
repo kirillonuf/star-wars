198fb9502eceb57f749f8f92aa2cc8da72ce4505
Kirill Onufriychuk <onufk1989@gmail.com>
2023-10-18 15:30:24 +0300

    added styles for person page

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,38 +1,28 @@
 import React, { Suspense, lazy, useEffect, useState } from "react";
-import { CircleLoader } from "react-spinners";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router";
 import PropTypes from "prop-types";
 
 import { withErrorApi } from "@hoc-helpers/withErrorApi";
-
 import { API_PERSON } from "@constants/api";
 import { getApiResource } from "@utils/network";
-
 import Button from "@ui/Button";
-
 import PersonDescription from "@components/PersonPage/PersonDescription";
-
 import PersonImg from "@components/PersonPage/PersonImg";
-
 
 import { themeLoader } from "@styles/styles";
 import styles from "./PersonPage.module.css";
 import Loading from "../../UI/Loading/Loading";
-import { useSelector } from "react-redux";
 
-const PersonFilms = lazy(()=> import("@components/PersonPage/PersonFilms"));
+const PersonFilms = lazy(() => import("@components/PersonPage/PersonFilms"));
 const PersonPage = ({ setErrorApi }) => {
   const navigate = useNavigate();
   const { id } = useParams();
-  // const storeData = useSelector(state => state.favoriteReducer)
 
-  
   const [person, setPerson] = useState(null);
   const [personName, setPersonName] = useState(null);
   const [personFilms, setPersonFilms] = useState(null);
 
-  
   useEffect(() => {
     (async () => {
       const res = await getApiResource(`${API_PERSON}/${id}/`);
@@ -55,30 +45,30 @@ const PersonPage = ({ setErrorApi }) => {
     })();
   }, []);
 
-  return (
-    person ? (
-      <div className={styles.container}>
-        <div className={styles.goBack}><Button onClick={() => navigate(-1)} name='Go Back' /></div>
-         
-        <span className={styles.star}></span>
-        <span className={styles.star2}></span>
-        <h1 className={styles.name}> {personName} </h1>
-        <PersonImg id={id} personName={personName} />
-        <div className={styles.info}>
-          <h3>Person info</h3>
-         {person &&  <PersonDescription person={person} />}
-          {personFilms &&
-          <Suspense fallback={<Loading theme={themeLoader.dark}/>}>
-            <PersonFilms films={personFilms}/>
-          </Suspense>
-
-          }
-        </div>
-        
+  return person ? (
+    <div className={styles.container}>
+      <div className={styles.goBack}>
+        <Button onClick={() => navigate(-1)} name="Go Back" />
       </div>
-    ): (
-      <Loading theme={themeLoader.dark} classes='dark'/>
-    )
+
+      <span className={styles.star}></span>
+      <span className={styles.star2}></span>
+      <h1 className={styles.name}> {personName} </h1>
+      <PersonImg id={id} personName={personName} />
+      <div className={styles.info}>
+        <h3>Person info</h3>
+        <div className={styles.person__info}>
+           {person && <PersonDescription person={person} />}
+        {personFilms && (
+          <Suspense fallback={<Loading theme={themeLoader.dark} />}>
+            <PersonFilms films={personFilms} />
+          </Suspense>
+        )}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading theme={themeLoader.dark} classes="dark" />
   );
 };
 
